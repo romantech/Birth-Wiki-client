99a43c25c233a911e60b17def9a9c971d15210ba
Sanghee Ma <sangheema@Sangheeui-MacBookPro.local>
2021-04-14 18:27:07 +0900

    modify

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -69,7 +69,7 @@ const SidebarsOpen = styled(Link)`
   display: flex;
   align-items: center;
   font-size: 30px;
-  margin 10px;
+  margin: 10px;
   position: absolute;
   right: 32px;
   height: 40px;
@@ -100,76 +100,14 @@ const NavSidebar = styled.div`
 `;
 
 const SidebarsClose = styled(Link)`
-display: flex;
-align-items: center;
-font-size: 30px;
-margin 10px;
-position: absolute;
-left: 32px;
-height: 40px;
-font-size: 2rem;
-background: none;
-color: #fff;
+  display: flex;
+  align-items: center;
+  font-size: 30px;
+  margin: 10px;
+  position: absolute;
+  left: 32px;
+  height: 40px;
+  font-size: 2rem;
+  background: none;
+  color: #fff;
 `;
-
-// const NavbarContainer = styled.div`
-//   display: flex;
-//   justify-content: space-between;
-//   height: 80px;
-//   z-index: 1;
-//   width: 100%;
-//   padding: 0 24px;
-//   max-width: 1100px;
-// `;
-
-// const NavLogo = styled(Link)`
-//   color: #fff;
-//   justify-self: flex-start;
-//   cursor: pointer;
-//   font-size: 1.5rem;
-//   display: flex;
-//   align-items: center;
-//   margin-left: 24px;
-//   font-weight: bold;
-//   text-decoration: none;
-// `;
-
-// const MobileIcon = styled.div`
-//   display: none;
-
-//   @media screen and (max-width: 768px) {
-//     display: block;
-//     position: absolute;
-//     top: 0;
-//     right: 0;
-//     transform: translate(-100%, 60%);
-//     font-size: 1.8rem;
-//     cursor: pointer;
-//     color: yellow;
-//   }
-// `;
-
-// const Navmenu = styled.ul`
-//   display: flex;
-//   align-items: center;
-//   list-style: none;
-//   text-align: center;
-//   margin-right: -22px;
-
-//   @media screen and (max-width: 768px) {
-//     display: none;
-//   }
-// `;
-
-// const NavItem = styled.li`
-//   height: 80px;
-// `;
-
-// const NavBtn = styled.nav`
-//   display: flex;
-//   align-items: center;
-
-//   @media screen and (max-width: 768px) {
-//     display: none;
-//   }
-// `;
