c636a0a29450732a6077dcb682be6c860ca69eed
G-Ryu <ckdals5815@gmail.com>
2021-04-21 17:46:42 +0900

    #83 / #84 / bugfix, refactoring

--- src/components/SidebarSignUp.tsx
+++ src/components/SidebarSignUp.tsx
@@ -184,14 +184,14 @@ function SidebarSignUp() {
               checkedEmail();
             }}
           />
-          {check.userEmail && duple.userEmail ? (
+          {/* {check.userEmail && duple.userEmail ? (
             <TestDiv>사용 가능</TestDiv>
           ) : check.userEmail ? (
             <TestDiv>이미 사용 중인 이메일입니다</TestDiv>
           ) : (
             <TestDiv>올바른 이메일을 입력해주세요</TestDiv>
-          )}
-          {/* {check.userEmail ? (
+          )} */}
+          {check.userEmail ? (
             <Valid to='#'>
               <ColorIcon.FcApproval />
             </Valid>
@@ -199,7 +199,7 @@ function SidebarSignUp() {
             <Invalid to='#'>
               <ColorIcon.FcCancel />
             </Invalid>
-          )} */}
+          )}
           <InputCatecory>닉네임</InputCatecory>
           <SignUpInput
             type='text'
